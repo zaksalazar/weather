370fb2458f0113c7c35f531e64d226fb3b3244ca
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2022-11-04 23:43:35 -0700

    added fetch weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,39 @@
-$(document).ready(function() {
-const city = document.getElementById("userinput").value;
-var weather = $(".weather");
-var temp = $(".temp");
-var description = $(".description");
-var humidity = $(".humidity");
-var wind = $(".wind");
-var searchButton = $("#submitButton");
+$(document).ready(function () {
+  const getCity = $("#userinput");
+  var city = "";
+  var weather = $(".weather");
+  var temp = $('#temp');
+  var description = $(".description");
+  var humidity = $(".humidity");
+  var wind = $(".wind");
+  var searchButton = $("#submitButton");
 
-//API call//
-var api = 'https://api.openweathermap.org/data/2.5/weather?q='; 
-var url =  'https://api.openweathermap.org/data/2.5/weather?q=London&appid=e8db7a3118009c80fecb82b6fe17e653'
-var APIKey = '&appid=21654ecd640e44008364d3ec8f18f916';
-var units = '&units=imperial'; 
+  //API call//
+  $("#submitButton").on("click", getCityName);
+  // code for enter
+  function getCityName(event) {
+    if (getCity.val().trim() !== "") {
+      city = getCity.val().trim();
 
-
-document.getElementById('submitButton').addEventListener("click", console.log(city)); 
-}); 
-    // code for enter
-
- 
-
-function getCity() {
-  console.log(city); }
-
-function getWeather() {
-     fetch(url)
-    .then((res) => res.json())
-    .then(function(response){
-        console.log(response)
+    }
   }
-    )}; 
-   
 
-//same day // 
-  
+  function getWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=21654ecd640e44008364d3ec8f18f916`)
+      .then((res) => res.json())
+      .then(function (response) {
+        console.log(response);
+        const tempF = response.main.temp;
+        tempF.html(tempF).tofixed(2)="&#8457"
+      });
+  }
 
-//take input from city and return weather data
 
-//display weather for city for the next 5 days
+  //same day //
 
-//store in history table
+  //take input from city and return weather data
 
+  //display weather for city for the next 5 days
+
+  //store in history table
+});
